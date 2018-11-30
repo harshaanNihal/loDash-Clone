37d633faadc66eaab631dfaf03e1ca478dda879f
klassynihal <harshaan.n.khan@gmail.com>
2018-11-30 10:46:27 +0530

    head.js

---
+++ array/head.js
@@ -0,0 +1,18 @@
+// Gets the first element of array.
+
+// ++++ARGUMENT++++
+// array(Array): The array to query.
+
+function head(array) {
+  return array[0];
+}
+
+// +++RETURN+++
+// (*): Returns the first element of array./
+
+
+// +++EXAMPLE+++
+// head([1, 2, 3]);
+// => 1
+// head([]);
+// => undefined
