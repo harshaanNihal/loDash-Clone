1a3ecd677ebf43aea8ed526e38966f0f65221202
klassynihal <harshaan.n.khan@gmail.com>
2018-12-06 17:16:35 +0530

    last.js

---
+++ array/last.js
@@ -0,0 +1,14 @@
+// Gets the last element of array.
+// ++++ARGUMENT++++
+// array(Array): The array to query.
+
+function last(array) {
+  return array[array.length - 1]
+}
+// +++RETURN+++
+// (*): Returns the last element of array
+
+
+// +++EXAMPLE+++
+// last([1, 2, 3]);
+// => 3
